94a7fd46b18c5cac1e03e2b5f7bb6828874c5e50
yeondu-0 <dpdls1021@gmail.com>
2025-08-22 14:46:52 +0900

    Revert "refactor: Do fizzbuzz with 1 Conditional Statement"
    This reverts commit 32d229c02232207a17413549aafa3189a2d18316.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,10 @@
-for j in range(1, 15+1):
-    if j % 3 == 0 or j % 5 == 0:
-       print('fizz' * (j % 3 == 0) + 'buzz' * (j % 5 == 0))
-    else:
+for j in range(1, 30+1):
+    if j % 3 == 0:
+        if j % 5 == 0:
+            print('fizzbuzz')
+        else:        
+            print('fizz')
+    elif j % 5 == 0:
+        print('buzz')
+    else: 
         print(j)
