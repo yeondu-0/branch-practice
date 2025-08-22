d7e6018573676696683ea973d8d3d82c9ce318d5
yeondu-0 <dpdls1021@gmail.com>
2025-08-22 10:47:42 +0900

    feat: Print 'buzz'

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
-for j in range(1, 17+1):
-    if j % 3 == 0:
+for i in range(1, 17+1):
+    if i % 3 == 0:
         print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
     else: 
-        print(j)
+        print(i)
